9acfe352e1069fade6a118dcb84013a74c6eafc9
RaikiKudo <45778339+RaikiKudo@users.noreply.github.com>
2019-01-01 23:20:11 +0900

    ログイン画面と新規登録画面のruby on rails をフォルダの中身全部あげました。

--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181226074224) do
-
-  create_table "boards", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "comments", force: :cascade do |t|
-    t.text "comment"
-    t.integer "user_id"
-    t.integer "board_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["board_id"], name: "index_comments_on_board_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
-  end
+ActiveRecord::Schema.define(version: 2018_12_23_064622) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
