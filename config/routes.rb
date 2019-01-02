fcacc3b7c193bda7e5172567fc3c79ea77a71a94
welldawn <42812546+welldawn@users.noreply.github.com>
2019-01-02 09:21:40 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,11 @@ Rails.application.routes.draw do
   post"/register",to:"pages#register_user"
   post"/login",to:"pages#login_check"
   get "/logout",to:"pages#logout"
+  
+  get 'board/create', to: "pages#thread_create"
+  post 'board/create', to: "pages#thread_create_process"
+
+  get 'board/:id', to: "pages#thread"
+  post 'board/:id', to: "pages#add_comment"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
